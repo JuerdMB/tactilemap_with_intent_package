3e61d7048e3ee276cb19400a491eb32f9d20bde6
Juerd <j.mispelblombeijer@student.tudelft.nl>
2023-08-15 11:11:55 +0200

    comments for framework

--- tmwi/src/tmwi_node.cpp
+++ tmwi/src/tmwi_node.cpp
@@ -9,8 +9,13 @@ int main(int argc, char **argv) {
     // Create Mapper that will take care of creating transformed, multi-scale maps of environment
     grid_map::GridMap global_map;
     Mapper mapper(nodeHandle, global_map);
-
     ros::Timer timer = nodeHandle.createTimer(ros::Duration(.5), std::bind(&Mapper::publishTransformedZoomedMap, &mapper));
+
+    // Check if there is already a database file present
+    // Wait for odometry to be found
+    // Then start running
+
+    // if(mode_localization) ask db file to publish as soon as odometry is found
 
     while(ros::ok()){
         ros::spinOnce();
